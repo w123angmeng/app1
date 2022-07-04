bbe9899c95b09292b189f0b71bd2d758fa950af0
w123angmeng <1521792736@qq.com>
2022-07-04 18:33:01 +0800

    app1 调用公共组件

--- src/main.js
+++ src/main.js
@@ -18,9 +18,11 @@ import VueRouter from 'vue-router';
 import App from './App.vue';
 import routes from './router';
 import store from './store';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
-
+Vue.use(ElementUI);
 let router = null;
 let instance = null;
 function render(props = {}) {
--- src/views/demo1.vue
+++ src/views/demo1.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
     <h1>This is app1-demo11</h1>
+    <el-button type="primary" @click="dialogData.visible = true">弹窗</el-button>
+    <!-- 公共组件 -->
+    <CommonDialog :dialogData="dialogData" @handleClose="handleClose"></CommonDialog>
   </div>
+
 </template>
+<script>
+export default {
+ data: () => {
+    return {
+        dialogData: {
+            visible: false,
+            content: "这是app1-调用父组件"
+        }
+    }
+ },
+ components: { 
+  CommonDialog: window.__POWERED_BY_QIANKUN__ ? window.commonComponent.CommonDialog :
+     import('@/components/HelloWorld.vue')
+},
+  methods: {
+    handleClose(data) {
+        console.log("app1 子组件监听到弹窗关闭", data)
+    }
+  }
+};
+</script>
